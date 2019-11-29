04bce66a3819f968636c2704d22460a7bb45df05
Massimo Triassi <massimo@plankdesign.com>
2019-11-28 20:42:49 -0500

    swap site name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
             <div class="row">
                 <div class="col-lg-8 col-md-10 mx-auto">
                     <div class="site-heading">
-                        <h1>Blog #392</h1>
+                        <h1>Wage Theory</h1>
                         <span class="subheading">A Blog about the year 2019</span>
                     </div>
                 </div>
