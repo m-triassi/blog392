3f07b6db77b2efc163b40d92de876f4b7e02ee48
Massimo Triassi <massimo@plankdesign.com>
2019-11-29 09:31:17 -0500

    hardcode link to article in about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -25,7 +25,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-            <p>This blog aims to inform the public about the terror of wage stagnation. If you'd like to know more about this issue, read <a href="/the-problem">this post</a> about the issue at hand.</p>
+            <p>This blog aims to inform the public about the terror of wage stagnation. If you'd like to know more about this issue, read <a href="article/the-problem">this post</a> about the issue at hand.</p>
             <p>In short, pay scaling hasn't kept up with inflation. This puts every generation, current and future at a disadvantage. If this issue isn't fixed soon, it could spell catastrophe.</p>
             <p>This blog aims to bring this issue to the attention of the broader public. It also explores potential solutions to this growing issue.</p>
         </div>
